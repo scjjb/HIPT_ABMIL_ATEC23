f04c392d997d15427c95719bbc4bed313b67d8b3
Jack Breen <77981425+scjjb@users.noreply.github.com>
2022-02-14 15:17:53 +0000

    Add GPU check
    Added a line to print whether torch is using cpu or cuda (gpu)

--- extract_features_fp.py
+++ extract_features_fp.py
@@ -16,6 +16,7 @@ from PIL import Image
 import h5py
 import openslide
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print("torch device:", device, "\n")
 
 def compute_w_loader(file_path, output_path, wsi, model,
  	batch_size = 8, verbose = 0, print_every=20, pretrained=True, 
