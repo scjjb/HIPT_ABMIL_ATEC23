eab608ee4bd698b72d8b9209faf5afb9c15d6cdc
Jack Breen <77981425+scjjb@users.noreply.github.com>
2022-10-10 16:01:15 +0100

    Add textural sampling, change dropout to float, add tasks
    Two texture models included - ['resnet50','levit_128s']
    
    New tasks - 'custom_1vsall_256_20x_aug','custom_1vsall_256_20x_1004'

--- eval.py
+++ eval.py
@@ -47,8 +47,7 @@ parser.add_argument('--model_size', type=str, choices=['small', 'big'], default=
                     help='size of model (default: small)')
 parser.add_argument('--model_type', type=str, choices=['clam_sb', 'clam_mb', 'mil'], default='clam_sb', 
                     help='type of model (default: clam_sb)')
-parser.add_argument('--drop_out', action='store_true', default=False, 
-                    help='whether model uses dropout')
+parser.add_argument('--drop_out', type=float, default=0.25, help='dropout p=0.25')
 parser.add_argument('--k', type=int, default=10, help='number of folds (default: 10)')
 parser.add_argument('--k_start', type=int, default=-1, help='start fold (default: -1, last fold)')
 parser.add_argument('--k_end', type=int, default=-1, help='end fold (default: -1, first fold)')
@@ -56,17 +55,19 @@ parser.add_argument('--fold', type=int, default=-1, help='single fold to evaluat
 parser.add_argument('--micro_average', action='store_true', default=False, 
                     help='use micro_average instead of macro_avearge for multiclass AUC')
 parser.add_argument('--split', type=str, choices=['train', 'val', 'test', 'all'], default='test')
-parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal',  'task_2_tumor_subtyping','custom','custom_1vsall','custom_1vsall_256_20x'])
+parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal',  'task_2_tumor_subtyping','custom','custom_1vsall','custom_1vsall_256_20x','custom_1vsall_256_20x_aug','custom_1vsall_256_20x_1004'])
 parser.add_argument('--profile', action='store_true', default=False, 
                     help='show profile of longest running code sections')
 parser.add_argument('--profile_rows', type=int, default=10, help='number of rows to show from profiler (requires --profile to show any)')
 parser.add_argument('--sampling', action='store_true', default=False, help='sampling for faster evaluation')
+parser.add_argument('--sampling_type', type=str, choices=['spatial','textural'],default='spatial',help='type of sampling to use')
 parser.add_argument('--samples_per_epoch', type=int, default=100, help='number of patches to sample per sampling epoch')
 parser.add_argument('--sampling_epochs', type=int, default=10, help='number of sampling epochs')
 parser.add_argument('--sampling_random', type=float, default=0.2, help='proportion of samples which are completely random per epoch')
 parser.add_argument('--sampling_random_delta',type=float, default=0.02, help='reduction in sampling_random per epoch')
 parser.add_argument('--sampling_neighbors', type=int, default=20, help='number of nearest neighbors to consider when resampling')
 parser.add_argument('--sampling_neighbors_delta', type=int, default=0, help='reduction in number of nearest neighbors per epoch')
+parser.add_argument('--texture_model',type=str, choices=['resnet50','levit_128s'], default='resnet50',help='model to use for feature extraction in textural sampling')
 parser.add_argument('--plot_sampling',action='store_true',default=False,help='Save an image showing the samples taken at each at last epoch')
 parser.add_argument('--plot_sampling_gif',action='store_true',default=False,help='Save a gif showing the evolution of the samples taken')
 parser.add_argument('--use_all_samples',action='store_true',default=False,help='Use every previous sample for final epoch')
@@ -161,6 +162,29 @@ elif args.task == 'task_2_tumor_subtyping':
                             patient_strat= False,
                             ignore=[])
 
+
+elif args.task == 'custom_1vsall_256_20x_aug':
+    args.n_classes=2
+    dataset =  Generic_MIL_Dataset(csv_path = 'dataset_csv/set_all_aug.csv',
+                            data_dir= os.path.join(args.data_root_dir, 'ovarian_dataset_features_256_patches_20x'),
+                            shuffle = False,
+                            print_info = True,
+                            label_dict = {'high_grade':0,'low_grade':1,'clear_cell':1,'endometrioid':1,'mucinous':1},
+                            patient_strat= False,
+                            ignore=[])
+
+
+elif args.task == 'custom_1vsall_256_20x_1004':
+    args.n_classes=2
+    dataset =  Generic_MIL_Dataset(csv_path = 'dataset_csv/set_all_1004.csv',
+                            data_dir= os.path.join(args.data_root_dir, 'ovarian_dataset_features_256_patches_20x'),
+                            shuffle = False,
+                            print_info = True,
+                            label_dict = {'high_grade':0,'low_grade':1,'clear_cell':1,'endometrioid':1,'mucinous':1},
+                            patient_strat= False,
+                            ignore=[])
+
+
 else:
     raise NotImplementedError
 
@@ -209,7 +233,9 @@ def main():
         model, patient_results, test_error, auc, df  = eval(split_dataset, args, ckpt_paths[ckpt_idx])
         all_results.append(all_results)
         all_auc.append(auc)
+        print("all auc", all_auc)
         all_acc.append(1-test_error)
+        print("all acc", all_acc)
         if not args.eval_features:
             df.to_csv(os.path.join(args.save_dir, 'fold_{}.csv'.format(folds[ckpt_idx])), index=False)	
 
