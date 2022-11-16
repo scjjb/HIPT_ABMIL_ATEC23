84f6f7a16a4819b0c95a1fe3f66663d0e122a5f1
root <jbreen8@hotmail.co.uk>
2022-11-16 21:59:03 +0000

    fixed B being too large for sampling
    B must be smaller than the number of available samples to avoid an error

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
             "reg": tune.loguniform(1e-8,1e-2),
             "drop_out": tune.uniform(0.0,0.99),
             "lr": tune.loguniform(5e-5,1e-3),
-            "B": tune.choice([4,6,16,32,64,128]),
+            "B": tune.choice([4,6,16,32,64]),
             "no_sample": tune.choice([0,10,20,30]),
             "weight_smoothing": tune.loguniform(0.001,0.5)}
         
