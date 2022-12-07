98c052966ad53da2a3e7c6e6486b7923b7d8c4d6
root <jbreen8@hotmail.co.uk>
2022-12-07 23:27:59 +0000

    bug fix in .to(device)

--- utils/core_utils_sampling.py
+++ utils/core_utils_sampling.py
@@ -560,7 +560,7 @@ def train_loop_sampling(epoch, model, loader, optimizer, n_classes, args, writer
             else:
                 sample_idxs=list(np.random.choice(range(0,len(coords)), size=total_samples_per_slide,replace=False))
                 all_sample_idxs=sample_idxs
-                data_sample=data[sample_idxs]#.to(device)
+                data_sample=data[sample_idxs].to(device)
             #data_sample, label = data_sample.to(device), label.to(device)
             logits, Y_prob, Y_hat, _, _ = model(data_sample)
 
