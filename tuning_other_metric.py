8b35ea13472d9227b86a41c1df8610430aa36a52
root <jbreen8@hotmail.co.uk>
2022-12-28 00:59:03 +0000

    add function for post-hoc trying different tuning metrics

---
+++ tuning_other_metric.py
@@ -0,0 +1,14 @@
+from ray import tune
+
+tuner = tune.Tuner.restore(
+        path="~/ray_results/test_run"
+    )
+
+results = tuner.fit()
+
+best_trial = results.get_best_result("loss", "min","avg")
+print("best trial:", best_trial)
+print("Best trial config: {}".format(best_trial.config))
+print("Best trial final loss: {}".format(best_trial.metrics["loss"]))
+print("Best trial final auc: {}".format(best_trial.metrics["auc"]))
+print("Best trial final acuracy: {}".format(best_trial.metrics["accuracy"]))
