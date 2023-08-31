cc40385bcd0aeeb1d0fd7a2438b4f02985a120ac
root <scjjb@leeds.ac.uk>
2023-08-31 15:39:57 +0000

    remove unused code
    pyramid_slides.py was instead implemented using vips in cmd, coords2pt.py was used to test whether there was a speed benefit in removing .h5 files but this wasn't found to be beneficial

--- pyramid_slides.py
+++
@@ -1,9 +0,0 @@
-import os
-vipsbin = r'i:\vips\vips-dev-8.14\bin'
-os.environ['PATH'] = vipsbin + ';' + os.environ['PATH']
-
-import pyvips
-
-tiffload("I:/treatment_data/2-1613704B.svs")
-tiffsave("I:/treatment_data/pyramid/2-1613704B.svs",compression=jpeg,pyramid=True,tile=True)
-
