6a1f0becd2fb52b14971f7135a0d8b25b6963b62
root <scjjb@leeds.ac.uk>
2023-09-08 10:47:20 +0000

    add tuning options
    doing three smaller tunings for ResNet-ABMIL as the larger models and slower training are harder to do large tunings on

--- main.py
+++ main.py
@@ -102,14 +102,22 @@ def main():
                     #    }
 
                     ## second ResNet-ABMIL tuning:
-                    search_space={
-                            "reg": tune.grid_search([0.001, 0.0001, 0.00001]),
-                            "drop_out": tune.grid_search([0.15, 0.35, 0.55]),
-                            "lr": tune.grid_search([0.005,0.001,0.0005]),
-                            "A_patches": tune.grid_search([6000, 5000, 4000 ]),
-                            "model_size": tune.grid_search(["small","tiny","tinier"])
-                            }
+                    #search_space={
+                    #        "reg": tune.grid_search([0.001, 0.0001, 0.00001]),
+                    #        "drop_out": tune.grid_search([0.15, 0.35, 0.55]),
+                    #        "lr": tune.grid_search([0.005,0.001,0.0005]),
+                    #        "A_patches": tune.grid_search([6000, 5000, 4000 ]),
+                    #        "model_size": tune.grid_search(["small","tiny","tinier"])
+                    #        }
 
+                    ## third ResNet-ABMIL tuning
+                    search_space={
+                            "reg": tune.grid_search([0.0001, 0.00001]),
+                            "drop_out": tune.grid_search([0.3, 0.4, 0.5]),
+                            "lr": tune.grid_search([0.001,0.0005]),
+                            "A_patches": tune.grid_search([10000, 8000, 6000]),
+                            "model_size": tune.grid_search(["tiny","tinier","tinier3"])
+                            }
 
             else:
                 if args.model_size in ["hipt_big","hipt_medium","hipt_small","hipt_smaller","hipt_smallest"]:
